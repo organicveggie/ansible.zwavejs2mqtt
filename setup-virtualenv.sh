fd8650d8cc2a093984074d154adb06c127d3e780
Sean Laurent <organicveggie@gmail.com>
2023-08-12 09:25:22 -0700

    Helper script to configure pyenv

--- setup-virtualenv.sh
+++ setup-virtualenv.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
-echo virtualenv venv
-virtualenv venv
-. venv/bin/activate
+PYENV_ROOT="$HOME/.pyenv"
+PATH="$PYENV_ROOT/bin:$PATH"
+PATH="$PYENV_ROOT/shims:${PATH}"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
-echo pip install --upgrade pip
+pversion=`pyenv latest 3`
+echo "Latest pyenv Python version is $pversion"
+
+venv="ansible-$pversion"
+echo "pyenv virtualenv $pversion $venv"
+pyenv virtualenv ${pversion} ${venv}
+
+pyenv activate ${venv}
+pyenv local ${venv}
+
+echo "pip install --upgrade pip"
 pip install --upgrade pip
 
-echo pip install ansible molecule ansible-lint molecule-vagrant molecule-lxd tox
-pip install ansible molecule ansible-lint molecule-vagrant molecule-lxd tox
+echo "pip install ansible ansible-navigator ansible-lint argcomplete"
+pip install ansible ansible-navigator ansible-lint argcomplete
+
+echo "pip install molecule \"molecule-plugins[vagrant]\" \"molecule-plugins[lxd]\" tox"
+pip install molecule "molecule-plugins[vagrant]" "molecule-plugins[lxd]" tox
